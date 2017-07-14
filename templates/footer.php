bd36e6b80500656ebb33434d6f94bb94e555f1fe
dhwlm <>
2017-07-14 22:35:16 +0530

    credit link added

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,7 @@
 
+    <a href="http://dhawalmehta.com" target="_blank" class="credit-link">2017 PHP Boilerplate by Dhawal Mehta</a>
+
+
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
